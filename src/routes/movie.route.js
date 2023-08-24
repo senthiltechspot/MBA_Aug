a0e3a01fdb89c002af707d4d21db534584059760
utmalik <utmalik@amazon.com>
2023-08-24 20:17:58 +0530

    Completed Movie CRUD

--- src/routes/movie.route.js
+++ src/routes/movie.route.js
@@ -1,5 +1,6 @@
-const { createMovie } = require("../controllers/movie.controller");
-const { verifyCreateTicketBody } = require("../middlewares/movie.middleware");
+const { createMovie, getMovies, getMovie, updateMovie, deleteMovie } = require("../controllers/movie.controller");
+const { verifyIdParam } = require("../middlewares/generic.middlware");
+const { verifyCreateMovieBody: verifyCreateTicketBody, verifyUpdateMovieBody } = require("../middlewares/movie.middleware");
 
 
 
@@ -8,5 +9,9 @@ const { verifyCreateTicketBody } = require("../middlewares/movie.middleware");
 module.exports =  function(app){
 
     app.post("/mba/api/v1/movies",[verifyCreateTicketBody],createMovie);
+    app.get("/mba/api/v1/movies",[], getMovies);
+    app.get("/mba/api/v1/movies/:id",[verifyIdParam], getMovie);
+    app.put("/mba/api/v1/movies/:id",[verifyIdParam, verifyUpdateMovieBody], updateMovie);
+    app.delete("/mba/api/v1/movies/:id",[verifyIdParam], deleteMovie);
 
 }
